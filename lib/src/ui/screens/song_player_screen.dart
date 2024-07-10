e76792fbfd3ca58aca67f557b51b635911182913
Tung Nguyen <nguyenvietminhtung0111@gmail.com>
2024-07-10 15:30:07 +0700

    Finished adding just_audio_background, Implemented WaveForm in just_audio_background, and fixed basic bugs related

--- lib/src/ui/screens/song_player_screen.dart
+++ lib/src/ui/screens/song_player_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:just_audio/just_audio.dart';
+import '../../services/services.dart';
 import '../components/components.dart';
 
 Scaffold songPlayerScreen(WidgetRef ref, ConcatenatingAudioSource playlist, int index) {
+  AppLifecycleListener(onResume: () => playlistSwitchState(ref));
   return Scaffold(
     appBar: songAppBar(ref),
     body: Column(
       children: <Widget>[
         songWaveForm(ref, true),
-        songNameBlock(ref, playlist[index] as UriAudioSource),
+        songNameBlock(ref),
         songProgressBar(ref, playlist, true),
         const SizedBox(height: 50),
         songIconBlock(ref, playlist, index,true),
